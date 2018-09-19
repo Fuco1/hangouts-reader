e70824ded86d36668f6a975e6b959aa5402d5c88
Matus Goljer <matus.goljer@gmail.com>
2018-09-19 12:59:38 +0200

    Fix the helper script to use the new output format

--- parse.sh
+++ parse.sh
@@ -4,4 +4,4 @@ INPUT=${2:-/dev/stdin}
 
 CONV_ID=$(grep "$1" "$INPUT" | head -n 1 | awk '{print $1}')
 
-grep $CONV_ID "$INPUT" | awk '{$1=""}1'
+grep $CONV_ID "$INPUT" | awk -F'\t' '{print "[" $2 "] " $3 ": " $4}'
