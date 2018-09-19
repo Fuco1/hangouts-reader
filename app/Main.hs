8f4046f0b1c6c81d2b0766d10453df9648be69e5
Matus Goljer <matus.goljer@gmail.com>
2018-09-19 12:42:42 +0200

    Use tab-separated line format by default

--- app/Main.hs
+++ app/Main.hs
@@ -2,56 +2,24 @@
 
 module Main where
 
-import Lib
+import Hangouts.Parser
+import Hangouts.Format
+
 import Data.Aeson
 import Data.Maybe (fromMaybe)
 import Data.List (intercalate, sortOn, find)
 import qualified Data.ByteString.Lazy as B
 import Data.Time.Format
 import Data.Time
-
-printConversationParticipants :: [Conversation] -> IO ()
-printConversationParticipants = mapM_
-  (\Conversation {conversationId = id, participants = p} ->
-      putStrLn $ id ++ ": " ++ formatParticipants p)
-
-printConvo :: Conversation -> IO ()
-printConvo c@Conversation {
-  conversationId = id
-  , participants = p
-  , events = e
-  } = do
-  tz <- getCurrentTimeZone
-  putStrLn $ (intercalate "\n" . map (formatEvent tz c) . sortOn time) e
-
-formatEvent :: TimeZone -> Conversation -> Event -> String
-formatEvent tz Conversation {
-  conversationId = convId
-  , participants = p
-  } Event {senderId = senderId, time = time, text = Just text} =
-  convId ++ " [" ++ formatTimestamp tz time ++ "] " ++ (getParticipantName senderId p) ++ ": " ++ text
-formatEvent _ _ Event {text = Nothing} = ""
-
-formatTimestamp :: TimeZone -> UTCTime -> String
-formatTimestamp tz time =
-  let local = utcToLocalTime tz time in
-    formatTime defaultTimeLocale "%c" local
-
-getParticipant :: String -> [Participant] -> Maybe Participant
-getParticipant chatId = find (\x -> participantId x == chatId)
-
-getParticipantName :: String -> [Participant] -> String
-getParticipantName chatId = fromMaybe "unknown" . (\p -> getParticipant chatId p >>= name)
-
-formatParticipants :: [Participant] -> String
-formatParticipants = intercalate ", " . map (fromMaybe "unknown" . name)
+import Data.Text.Prettyprint.Doc.Render.Text (putDoc)
 
 main :: IO ()
 main = do
   json <- B.getContents
-  let convos = conversations <$> decode json
+  tz <- getCurrentTimeZone
+  let config = Config tz
+  let convos = decode json
   case convos of
     Just convos' -> do
-      printConversationParticipants convos'
-      mapM_ printConvo convos'
+      putDoc $ format config convos'
     Nothing -> putStrLn "error"
--- parse.sh
+++ parse.sh
@@ -2,6 +2,6 @@
 
 INPUT=${2:-/dev/stdin}
 
-CONV_ID=$(grep "$1" "$INPUT" | head -n 1 | awk '{print $1}' | tr -d ':')
+CONV_ID=$(grep "$1" "$INPUT" | head -n 1 | awk '{print $1}')
 
 grep $CONV_ID "$INPUT" | awk '{$1=""}1'
