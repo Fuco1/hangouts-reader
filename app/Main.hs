6c7f015e6b8ea047f6a3e4f2ef770918d072fd7e
Matus Goljer <matus.goljer@gmail.com>
2018-09-19 10:44:16 +0200

    Print the convo IDs at the beginning and then on each line

--- app/Main.hs
+++ app/Main.hs
@@ -9,18 +9,22 @@ import Data.List (intercalate, sortOn)
 import qualified Data.ByteString.Lazy as B
 import Data.Time.Format
 
+printConversationParticipants :: [Conversation] -> IO ()
+printConversationParticipants = mapM_
+  (\Conversation {conversationId = id, participants = p} ->
+      putStrLn $ id ++ ": " ++ formatParticipants p)
+
 printConvo :: Conversation -> IO ()
 printConvo Conversation {
   conversationId = id
   , participants = p
   , events = e
-  } = putStrLn $ id ++ ": " ++ formatParticipants p ++
-  "\n=========\n" ++ (intercalate "\n" . map formatEvent . sortOn time ) e
+  } = putStrLn $ (intercalate "\n" . map (formatEvent id) . sortOn time ) e
 
-formatEvent :: Event -> String
-formatEvent Event {senderId = senderId, time = time, text = Just text} =
-  "[" ++ formatTime defaultTimeLocale "%c" time ++ "] " ++ senderId ++ ": " ++ text
-formatEvent Event {text = Nothing} = ""
+formatEvent :: String -> Event -> String
+formatEvent convId Event {senderId = senderId, time = time, text = Just text} =
+  convId ++ " [" ++ formatTime defaultTimeLocale "%c" time ++ "] " ++ senderId ++ ": " ++ text
+formatEvent _ Event {text = Nothing} = ""
 
 formatParticipants :: [Participant] -> String
 formatParticipants = intercalate ", " . map (fromMaybe "unknown" . name)
@@ -30,5 +34,7 @@ main = do
   json <- B.getContents
   let convos = conversations <$> decode json
   case convos of
-    Just convos' -> mapM_ printConvo convos'
+    Just convos' -> do
+      printConversationParticipants convos'
+      mapM_ printConvo convos'
     Nothing -> putStrLn "error"
